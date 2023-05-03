2f0a39507fdd391cf7407b93dec43608a01009b4
Douglas França <douglasbragaf@gmail.com>
2023-05-03 11:01:03 -0300

    Organizando as pastas do dia 3.1

---
+++ fundamentos/secao-03-Introducao-JavaScript/dia-1-primeiros-passos/exercicio-scripts.js
@@ -0,0 +1,45 @@
+let a = 10;
+let b = 20;
+let c = 30;
+
+let soma = a + b;
+    console.log(soma);
+let subtracao = a - b;
+    console.log(subtracao);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+
+if (a>b){
+    console.log(a);
+}
+else {
+    console.log(b);
+}
+
+if (a>b && a>c){
+    console.log(a);
+}
+else if (b>a && b>c){
+    console.log(b);
+}
+else{
+    console.log(c);
+}
+
+if (c >0){
+    console.log("positive");
+}
+else if (c<0){
+    console.log("negative");
+}
+else{
+    console.log("zero");
+}
+
+if (a+b+c === 180){
+    console.log();
+}
+else {
+    console.log();
+}
--- fundamentos/secao-03-Introducao-JavaScript/dia-1-primeiros-passos/para-fixar/fixar-1-scripts.js
+++ fundamentos/secao-03-Introducao-JavaScript/dia-1-primeiros-passos/para-fixar/fixar-1-scripts.js
@@ -1,5 +1,13 @@
-Para Fixar:
+/*Para Fixar:
 1 - Crie uma variável chamada myName e atribua a ela o seu nome (Exemplo: Carolina).
 2 - Crie uma variável chamada birthCity e atribua a ela a sua cidade natal.
 3 - Crie uma variável chamada birthYear e atribua a ela o ano em que você nasceu.
-4 - Utilize o console.log() para imprimir as variáveis que você criou.
+4 - Utilize o console.log() para imprimir as variáveis que você criou.*/
+
+let myName = "Douglas";
+const birthCity = "Petrópolis";
+const birthYear = 1995;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);

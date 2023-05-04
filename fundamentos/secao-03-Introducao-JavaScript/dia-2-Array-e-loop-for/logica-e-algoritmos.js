d22601cafa6724a7c9db2b0ed6a4d3d02454b525
Douglas França <douglasbragaf@gmail.com>
2023-05-04 10:32:35 -0300

    Palavra invertida

--- fundamentos/secao-03-Introducao-JavaScript/dia-2-Array-e-loop-for/logica-e-algoritmos.js
+++ fundamentos/secao-03-Introducao-JavaScript/dia-2-Array-e-loop-for/logica-e-algoritmos.js
@@ -2,7 +2,16 @@
 let num = 10;
 let fatoracao = 1;
 
-for (i = num; i > 0; i -= 1){
+for (let i = num; i > 0; i -= 1){
     fatoracao *= i; ;
     console.log(`${i} = ${fatoracao}`);
 }
+
+/*Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra, como a palavra “banana” para “ananab”. Utilize a string abaixo como exemplo. Depois, troque-a por outras para verificar se seu algoritmo está funcionando corretamente.*/
+let word = 'tryber';
+let palavraInvertida = "";
+
+for (let index = 0; index < word.length; index += 1){
+    palavraInvertida += word[word.length - 1 - index]; 
+}
+console.log(palavraInvertida);

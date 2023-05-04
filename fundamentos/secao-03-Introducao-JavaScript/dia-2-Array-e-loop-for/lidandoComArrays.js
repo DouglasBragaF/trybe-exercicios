499fdd392aa361a07356a4a1fb4c7eab7fee4ea5
Douglas França <douglasbragaf@gmail.com>
2023-05-04 08:23:54 -0300

    Requisitos 1 e 2

---
+++ fundamentos/secao-03-Introducao-JavaScript/dia-2-Array-e-loop-for/lidandoComArrays.js
@@ -0,0 +1,12 @@
+//Percorra o array imprimindo todos os valores contidos nele com a função console.log()//
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+for (let index = 0; index < numbers.length; index += 1){
+    console.log(numbers[index]);
+}
+
+//Some todos os valores contidos no array e imprima o resultado.//
+let soma =0;
+for (let i = 0; i < numbers.length; i += 1){
+    soma += numbers[i];
+}
+console.log(soma);
